d272b3f8661eacab14e44fc7e24ddbe1ac9ff495
John Howard <longhornjoho@gmail.com>
2017-02-25 21:17:56 -0600

    build api server and serve

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
+import 'rxjs/add/operator/map';
+
 if (environment.production) {
   enableProdMode();
 }
