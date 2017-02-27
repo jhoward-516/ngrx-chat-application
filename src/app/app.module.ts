d4e2ab7db24344bfef3a2bedc036fbb5fe7fa2aa
John Howard <longhornjoho@gmail.com>
2017-02-26 21:22:31 -0600

    build store logic, action, and reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,45 @@ import { MessageSectionComponent } from './message-section/message-section.compo
 import { ThreadListComponent } from './thread-list/thread-list.component';
 import { MessageListComponent } from './message-list/message-list.component';
 import {ThreadsService} from "./services/threads.service";
+import {StoreModule, Action} from "@ngrx/store";
+import {INITIAL_APPLICATION_STATE, ApplicationState} from "./store/application-state";
+import {LOAD_USER_THREADS_ACTION, LoadUserThreadsAction} from "./store/actions";
+import * as _ from 'lodash';
+
+
+export function storeReducer(state: ApplicationState = INITIAL_APPLICATION_STATE,
+                      action: Action): ApplicationState {
+
+  switch (action.type) {
+
+    case LOAD_USER_THREADS_ACTION:
+      return handleLoadUserThreadsAction(state, action);
+
+
+    default:
+      return state;
+
+  }
+
+}
+
+function handleLoadUserThreadsAction(state: ApplicationState,
+  action: LoadUserThreadsAction): ApplicationState {
+
+  const userDate = action.payload;
+
+  const newState: ApplicationState = Object.assign({}, state);
+
+  newState.storeData = {
+    participants: _.keyBy(action.payload.participants, 'id'),
+    messages: _.keyBy(action.payload.messages, 'id'),
+    threads: _.keyBy(action.payload.threads, 'id'),
+  };
+
+  return newState;
+
+}
+
 
 @NgModule({
   declarations: [
@@ -23,7 +62,8 @@ import {ThreadsService} from "./services/threads.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+      StoreModule.provideStore(storeReducer)
   ],
   providers: [ThreadsService],
   bootstrap: [AppComponent]
---
+++ src/app/store/UiState.ts
@@ -0,0 +1,14 @@
+
+export interface UiState {
+
+  userId: number;
+  currentThreadId: number
+
+}
+
+export const INITIAL_UI_STATE: UiState = {
+
+  userId: undefined,
+  currentThreadId: undefined
+
+};
